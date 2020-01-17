100f938be3d2d39806e0062ddaa08f4dcd307936
MauroEstay <mauroestay@gmail.com>
2020-01-17 02:19:55 -0300

    add task count

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    if user_signed_in?
+      @userTask = current_user.tasks
+    end
   end
 
   def completed
@@ -13,8 +16,13 @@ class TasksController < ApplicationController
     end
     redirect_to root_path
   end
+
   def show
+    @tasks = Task.all
     @task = Task.find(params[:id])
     @rank = @task.users.order(:created_at).limit(5)
+    if user_signed_in?
+      @userTask = current_user.tasks
+    end
   end
 end
